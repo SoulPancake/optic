255f426b6d841617e9d4dc7f8ca212bb6cd14c15
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-12-24 17:09:09 +0530

    adding modal to sync nest component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState } from "react";
 import axios from "axios";
-import { Star } from "@material-ui/icons";
+import {  Star } from "@material-ui/icons";
 import "./app.css";
 import Avatar from "@material-ui/core/Avatar";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
 import {format} from 'timeago.js';
-import CoronavirusIcon from '@mui/icons-material/Coronavirus';
+
 function App() {
   const [pins, setPins] = useState([]);
+  const [currentPlaceID,setCurrentPlaceID] = useState(null);
   const [viewstate, setViewstate] = useState({
     width: "100vw",
     height: "100vh",
@@ -30,7 +34,29 @@ function App() {
     getPins();
   }, []);
 
-  // const [showPopup, setShowPopup] = React.useState(true);
+
+   const handleMarkerClick = (id,lat,long) => {
+          setCurrentPlaceID(lat,long);
+          if(showPopup) setShowPopup(false);
+          else setShowPopup(true)
+  };
+
+  const openModal = (lat,long) =>{
+      console.log("Open Modal called" , lat,long)
+      handleOpen()
+  }
+
+   const [showPopup, setShowPopup] = React.useState(false);
+
+   const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
   return (
     <Map
       initialViewState={viewstate}
@@ -44,48 +70,73 @@ function App() {
           <Marker 
             longitude={p.long} 
             latitude={p.lat} 
+            
+           
           >
 
-           //https://youtu.be/9oEQvI7K-rA?t=3898
 
 
           </Marker>
 
-          <Popup 
-            longitude={p.long} 
-            latitude={p.lat} 
-            anchor="bottom">
-            <div className="card">
-              <label>Place</label>
-              <h4 className="place">Eiffel Tower</h4>
-              <label>Type</label>
-              <p className="desc">{p.description}</p>
+      
+               
 
-              <label>
-                Join Nest
-                <Avatar
-                  className="syncNestLogo"
-                  alt="SyncNest - Logo"
-                  src={require(".//SyncNest.png")}
-                />
-                <br></br>
-              </label>
 
-              <label>Critical Level</label>
-              <div className="stars">
-                <Star className="star" />
-                <Star className="star" />
-                <Star className="star" />
-                <Star className="star" />
+         
+            <Popup 
+              longitude={p.long} 
+              latitude={p.lat} 
+              anchor="bottom">
+              <div className="card">
+                <label>Place</label>
+                <h4 className="place">Eiffel Tower</h4>
+                <label>Type</label>
+                <p className="desc">{p.description}</p>
+  
+                <label>
+                  Join Nest
+                  <Avatar
+                    className="syncNestLogo"
+                    alt="SyncNest - Logo"
+                    src={require(".//SyncNest.png")}
+                    onClick={()=>openModal(p.lat,p.long)}
+                  />
+                  <br></br>
+                </label>
+  
+                <label>Critical Level</label>
+                <div className="stars">
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                </div>
+                <label>Information </label>
+                <span className="username">
+                  Created by <b>{p.username}</b>
+                </span>
+                <span className="date">{format(p.createdAt)}</span>
               </div>
-              <label>Information </label>
-              <span className="username">
-                Created by <b>{p.username}</b>
-              </span>
-              <span className="date">{format(p.createdAt)}</span>
-            </div>
-          </Popup>
-        </>
+            </Popup> 
+           
+         <Modal
+        hideBackdrop
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ width: 200 }}>
+          <h2 id="child-modal-title">Text in a child modal</h2>
+          <p id="child-modal-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+         
+        </Box>
+      </Modal>
+        
+          
+        </ >
       ))}
     </Map>
   );
